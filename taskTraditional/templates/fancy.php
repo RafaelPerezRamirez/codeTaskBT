04d9ed20ad5e432cf776ada65bc9564b6a14cb7d
rafabrutaldrums <rafael@gafa.mx>
2017-04-05 10:38:09 -0500

    include traditional version

---
+++ taskTraditional/templates/fancy.php
@@ -0,0 +1,23 @@
+<div class="fancy-comments">
+    <div class="close-fancy"></div>
+    <div class="main-fancy-content">
+        <div class="header-fancy">
+            <div class="inline middle image-fancy"></div>
+            <div class="inline middle content-body">
+            </div>
+        </div>
+        <div class="content-comments">
+            <div class="header-comments">
+                <div class="inline middle comments-header-id">
+                    id
+                </div><!--
+             --><div class="inline middle comments-header-title">
+                    title
+                </div><!--
+             --><div class="inline middle comments-header-body">
+                    body
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ taskTraditional/templates/menu.php
@@ -0,0 +1,16 @@
+<div class="header-main">
+    <div class="menu-layout">
+        <div class="roboto title-menu">
+            Codetask
+        </div>
+        <div class="icon-buttons">
+            <div class="button-search pointer button-header">
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </div>
+            <div class="button-close pointer button-header hidden">
+                <i class="fa fa-times" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+    <input type="text" class="input-found" placeholder="Busqueda">
+</div>
